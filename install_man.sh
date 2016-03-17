cfc465b85ed27ffa74d6feec5bba488edd7a404c
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-03-17 13:34:29 +0000

    Implement Valentine

---
+++ install_man.sh
@@ -0,0 +1,3 @@
+#!bin/sh
+
+./main.native --help=groff > $(opam config var man)/man1/valentine.1
---
+++ main.ml
@@ -0,0 +1,42 @@
+open StdLabels
+open Cmdliner
+
+let oops msg =
+  print_endline "You have a mistake in your HTML";
+  Printf.sprintf "\027[31m%s\027[m" msg
+  |> print_endline
+
+let html_files =
+  let doc = "List of html files to validate" in
+  Arg.(non_empty & pos_all file [] & info [] ~docv:"HTML file(s)" ~doc)
+
+let do_parse html_file =
+  Markup.(
+    file html_file
+    |> fun (stream, closer) ->
+    stream
+    |> parse_html
+      ~report:(fun location e -> Markup.Error.to_string ~location e |> oops)
+    |> signals
+    |> write_html
+    |> to_string
+    |> fun _ -> closer ()
+  )
+
+let begin_program
+    html_files =
+  html_files |> List.iter ~f:do_parse
+
+let entry_point =
+  Term.(pure
+          begin_program
+       $ html_files)
+
+let top_level_info =
+  let doc = "Validate your HTML according to HTML spec" in
+  let man = [`S "DESCRIPTION"] in
+  Term.info ~version:"0.9.0" ~doc ~man "valentine"
+
+let () =
+  Term.eval (entry_point, top_level_info)
+  |> ignore
